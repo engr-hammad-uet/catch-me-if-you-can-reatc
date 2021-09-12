5f27a0d5fb3cb77090da6094cf4b8f3ac1f52534
Hammad Ul Hassan <64351269+engr-hammad-uet@users.noreply.github.com>
2021-09-11 23:44:20 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,44 +5,6 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 
-
-
-
-class catchMe extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            posX: 0,
-        };
-      }
-  
-
-    handleMouseMoveCatch = (e) => {
-        this.setState({ posX: this.posX=Math.random() * 100 +'%' })
-    }
-    jewelStyle = function() {
-        return {
-            top :this.posX=Math.random() * 95 +'%' ,
-            left :this.posX=Math.random() * 95 +'%' ,
-        }
-      }
-
-    render() {
-        return (
-            <div>
-                <div style={this.jewelStyle()} onMouseMove={this.handleMouseMoveCatch} className="outer-block">
-                    <p className="inner-block">Catch me if you can</p>
-                </div>
-            </div>
-        )
-    }
-}
-
-
-
-
-
-
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
